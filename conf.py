19ca48a9305a232aa596a6701cfeeeb0eaaac8a8
Reedo0910 <sea.sand@live.cn>
2019-12-29 20:56:47 -0500

    update icon

--- conf.py
+++ conf.py
@@ -61,6 +61,10 @@ social_links = [
 head_addon = r'''
 <meta http-equiv="x-dns-prefetch-control" content="on">
 <link rel="dns-prefetch" href="//cdn.jsdelivr.net" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="shortcut icon" href="/favicon.ico">
 '''
 
 footer_addon = ''
